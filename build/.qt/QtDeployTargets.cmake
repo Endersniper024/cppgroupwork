faf17432e608c9b2734af11618770d965f15614b
Endersniper024 <Paulchenjiale@hotmail.com>
2025-05-29 12:10:31 +0800

    Clean repository: keep only src/, CMakeLists.txt, and README

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_cppgroupwork_FILE D:/git_repos/cppgroupwork-2/build/cppgroupwork.exe)
-set(__QT_DEPLOY_TARGET_cppgroupwork_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_cppgroupwork_RUNTIME_DLLS D:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Sql.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Network.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;D:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
